d73d17008b578f40675ab5d4b2ecaa3fb12b98b6
milu <elk820925@gmail.com>
2024-08-23 16:25:57 +0800

    排名

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import MainLayout from "@/components/layout/main-layout";
-// 首頁 - 廣告牆
+
 import Advertise from "@/components/home/advertise";
 import HotList from "@/components/home/hot-list";
 
 export default function Home() {
+  const domain = process.env.NEXT_PUBLIC_DOMAIN;
+  const [hotList, setHotList] = useState([]);
+
+  useEffect(() => {
+    getdata();
+  }, []);
+  const getdata = async () => {
+    try {
+      const { data } = await axios.get(`${domain}/product/top`);
+      setHotList(data.data);
+    } catch (error) {
+      console.log(error, "index.js error");
+    }
+  };
+
   return (
     <MainLayout>
       <Advertise />
-      <HotList/>
+      <HotList data={hotList} />
     </MainLayout>
   );
 }
