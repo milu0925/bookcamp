643cb8be99e5757aad2ae330513327e88d30c8a0
milu0925 <milu17y@gmail.com>
2024-10-11 16:29:04 +0800

    FF

--- components/book/all_book.js
+++ components/book/all_book.js
@@ -61,28 +61,32 @@ export default function AllBook() {
         <></>
       </div>
       <div className={style.r_books}>
-        {books.map((v, i) => (
-          <div key={v.id} data-id={v.id}>
-            <div className={`${style.r_book} pixel-border-white bg-white`}>
-              <img alt={`${v.b_title}-${i}`} src={`/images/book/${v.b_img}`} />
-              <div className={style.r_book_title}>{v.b_title}</div>
-              <div className="price-area">
-                <div className="pixel-d-purple bg-dark-purple">
-                  <MdAttachMoney />
-                  {v.b_price}元
+        {books &&
+          books.map((v, i) => (
+            <div key={v.id} data-id={v.id}>
+              <div className={`${style.r_book} pixel-border-white bg-white`}>
+                <img
+                  alt={`${v.b_title}-${i}`}
+                  src={`/images/book/${v.b_img}`}
+                />
+                <div className={style.r_book_title}>{v.b_title}</div>
+                <div className="price-area">
+                  <div className="pixel-d-purple bg-dark-purple">
+                    <MdAttachMoney />
+                    {v.b_price}元
+                  </div>
+                  <button
+                    className="pixel-border-add"
+                    onClick={() => {
+                      handleAddCart(v);
+                    }}
+                  >
+                    <FaCartPlus />
+                  </button>
                 </div>
-                <button
-                  className="pixel-border-add"
-                  onClick={() => {
-                    handleAddCart(v);
-                  }}
-                >
-                  <FaCartPlus />
-                </button>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
       <div className={style.r_book_pagination}></div>
     </div>
--- components/home/hot-list.js
+++ components/home/hot-list.js
@@ -73,46 +73,47 @@ export default function HotList() {
       </div>
       <div className={`${style.col_hotlist}`}>
         <Slider {...settings} className={style.custom_slider}>
-          {books.map((v, index) => (
-            <div key={v.b_id} className={style.hotlist_card}>
-              <div>
-                <span className={style.hotlist_card_rank}>
-                  <img alt="rank" src="/images/icon-pixel/no1_1.svg" />
-                  <span className="border-pixel-w">
-                    <div className="font-s">No.</div>
-                    {index + 1}
+          {books &&
+            books.map((v, index) => (
+              <div key={v.b_id} className={style.hotlist_card}>
+                <div>
+                  <span className={style.hotlist_card_rank}>
+                    <img alt="rank" src="/images/icon-pixel/no1_1.svg" />
+                    <span className="border-pixel-w">
+                      <div className="font-s">No.</div>
+                      {index + 1}
+                    </span>
                   </span>
-                </span>
 
-                <div
-                  className={`${style.hotlist_card_content} pixel-border-purple bg-bright-purple`}
-                >
-                  <Link href="/">
-                    <img alt="book" src="/images/book/lover.png" />
-                  </Link>
-                  <div className={style.hotlist_btn_group}>
-                    <div className={style.sale_block}>
-                      銷售量<span>{v.b_sales}</span>本
-                    </div>
-                    <div className="price-area">
-                      <div className="pixel-d-purple bg-dark-purple">
-                        <MdAttachMoney />
-                        {v.b_price}元
+                  <div
+                    className={`${style.hotlist_card_content} pixel-border-purple bg-bright-purple`}
+                  >
+                    <Link href="/">
+                      <img alt="book" src="/images/book/lover.png" />
+                    </Link>
+                    <div className={style.hotlist_btn_group}>
+                      <div className={style.sale_block}>
+                        銷售量<span>{v.b_sales}</span>本
                       </div>
-                      <button
-                        className="pixel-border-add"
-                        onClick={() => {
-                          handleAddCart(v);
-                        }}
-                      >
-                        <FaCartPlus />
-                      </button>
+                      <div className="price-area">
+                        <div className="pixel-d-purple bg-dark-purple">
+                          <MdAttachMoney />
+                          {v.b_price}元
+                        </div>
+                        <button
+                          className="pixel-border-add"
+                          onClick={() => {
+                            handleAddCart(v);
+                          }}
+                        >
+                          <FaCartPlus />
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
         </Slider>
       </div>
       <div
