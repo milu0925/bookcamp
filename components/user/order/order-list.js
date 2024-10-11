4832153e940cb448856e169300d321de72ab31c4
milu0925 <milu17y@gmail.com>
2024-10-11 11:35:11 +0800

    WW

--- components/user/order/order-list.js
+++ components/user/order/order-list.js
@@ -62,8 +62,8 @@ export default function OrderList() {
               <div>{v.o_id}</div>
               <div>{v.o_create_date}</div>
               <div>{v.o_total}</div>
-              <div>{v.pay === 1 ? "linepay" : "貨到付款"}</div>
-              <div>{v.o_status === 1 ? "已付款" : "未付款"}</div>
+              <div>{v.pay == 1 ? "linepay" : "貨到付款"}</div>
+              <div>{v.o_status == 1 ? "已付款" : "未付款"}</div>
               <div className={style.fn_group}>
                 <button
                   className={style.view_detail}
